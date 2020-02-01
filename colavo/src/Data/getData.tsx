6dec362c172f6431c7f5e1a832bba38b3129a8e0
황종옥 <jongokhwng@gmail.com>
2020-02-01 17:18:37 +0900

    :sparkles: Add getData.js
    fetch API 모듈화

--- colavo/src/Data/getData.tsx
+++ colavo/src/Data/getData.tsx
@@ -1,12 +1,5 @@
-export default async function getData(){
+export default async function getData(set:any){
   let data = await fetch("https://us-central1-colavolab.cloudfunctions.net/requestAssignmentCalculatorData")
       .then(res => res.json())
-
-  let items = data.items
-  let discounts = data.discounts
-  let currencyCode = data.currency_code
-
-  let dataArr = [items, discounts, currencyCode]
-
-  return dataArr
+  set(data)
 }

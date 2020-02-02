f65c20cf9a30b9188dd2231250cc845f776506fb
황종옥 <jongokhwng@gmail.com>
2020-02-02 22:14:18 +0900

    :construction: Working select items render

--- colavo/src/Pages/Basket/Basket.tsx
+++ colavo/src/Pages/Basket/Basket.tsx
@@ -1,28 +1,34 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
+import ItemsModal from '../../Components/Modal/ItemsModal'
+import DiscountModal from '../../Components/Modal/DiscountModal'
 import getData from '../../Data/getData'
 import {Body} from './stlye'
-import {Modal} from 'antd'
+
+type Data = {
+  items: object
+  discounts: object
+  currency_code: string
+}
 
 export default function Basket() {
-  const [show, setshow] = useState(false)
-  
-  console.log(getData())
+  const [showItems, setshowItems] = useState<boolean>(false)
+  const [showDiscounts, setshowDiscounts] = useState<boolean>(false)
+  const [select, setselect] = useState<object>({})
+  const [dataObj, setdataObj] = useState<Data>({
+    items:{},
+    discounts:{},
+    currency_code:""
+  })
+
+  useEffect(() => {getData(setdataObj)},[])
+
   return (
     <Body>
       <div>테스트</div>
-      <button onClick={() => setshow(true)}>시술</button>
-      <Modal
-          title="Vertically centered modal dialog"
-          centered
-          visible={show}
-          onOk={() => setshow(false)}
-          onCancel={() => setshow(false)}
-        >
-          <p>some contents...</p>
-          <p>some contents...</p>
-          <p>some contents...</p>
-        </Modal>
-      <button>할인</button>
+      <button onClick={() => setshowItems(true)}>시술</button>
+      <ItemsModal show={showItems} setshow={setshowItems} Data={dataObj.items} setselect={setselect} title="시술"/>
+      <button onClick={()=> setshowDiscounts(true)}>할인</button>
+      <DiscountModal show={showDiscounts} setshow={setshowDiscounts} Data={dataObj.discounts} setselect={setselect} title="할인"/>
       <div>
         <span>
           합계

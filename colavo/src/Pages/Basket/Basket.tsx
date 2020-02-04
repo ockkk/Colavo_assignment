92bc5e8d86e87b00229ae45ac287fdb26acd9b6f
황종옥 <jongokhwng@gmail.com>
2020-02-04 15:50:59 +0900

    :lipstick: Modify Basket UI

--- colavo/src/Pages/Basket/Basket.tsx
+++ colavo/src/Pages/Basket/Basket.tsx
@@ -3,7 +3,8 @@ import ItemsModal from '../../Components/Modal/ItemsModal'
 import DiscountModal from '../../Components/Modal/DiscountModal'
 import Counter from '../../Components/Counter/Counter'
 import getData from '../../Data/getData'
-import { Body, UserNameBox, ButtonBox, Antdbutton } from './stlye'
+import { Body, UserNameBox, ButtonBox, Antdbutton, BasketBox, FooterBox, Text } from './stlye'
+import {CardBody }from '../../Components/Card/style'
 import DiscountItemsDropdown from '../../Components/Dropdown/DiscountItemsDropdown'
 
 type Data = {
@@ -27,6 +28,7 @@ export default function Basket() {
 
   const [total, settotal] = useState("0")
   let discountItemList
+
   const renderTotal = () => {
     let itemStorage:any = localStorage.getItem("items")
     let itemList = JSON.parse(itemStorage)
@@ -39,10 +41,10 @@ export default function Basket() {
         sum += Number(val["price"]) * Number(val["count"])
       })
     }
-
+    console.log("실행 되니 ?")
     if(discountList){
+      console.log("[TOTAL]",discountList)
       discountList.map((val:{[index:string]:string})=>{
-        console.log(val)
         sum -= Number(val["discount"])
       })
     }
@@ -57,47 +59,50 @@ export default function Basket() {
     renderTotal()
   },[selectItems,selectDiscount])
 
-  console.log("[ITEMS]",selectItems)
-  console.log("[DISCOUNT]", selectDiscount)
   return (
     <Body>
       <UserNameBox>
         <h1>
-          테스트
+          ockkk
         </h1>
       </UserNameBox>
       <ButtonBox>
         <Antdbutton onClick={() => setshowItems(true)}>시술</Antdbutton>
         <ItemsModal show={showItems} setshow={setshowItems} Data={dataObj.items} setselectItems={setselectItems} renderTotal={renderTotal} title="시술"/>
+        <span style={{padding:"15px"}}/>
         <Antdbutton onClick={()=> setshowDiscounts(true)}>할인</Antdbutton>
         <DiscountModal show={showDiscounts} setshow={setshowDiscounts} Data={dataObj.discounts} setselectDiscount={setselectDiscount} renderTotal={renderTotal} title="할인"/> 
       </ButtonBox>
-      <div style={{height: "70%"}}>
+      <BasketBox>
         {selectItems.map((val:{[index:string]:string}, index) => 
-        <div key={index}>
-          <p>{val["name"]}</p>
-          <p>{val["price"]}</p>
+        <CardBody key={index}>
+          <div>
+          <span>{val["name"]}</span>
           <Counter name={val["name"]} title="바구니" renderTotal={renderTotal}/>
-        </div>
+          </div>
+          <p>{val["price"]}</p>
+        </CardBody>
         )}
         {selectDiscount.map((val:{[index:string]:string}, index:any) => 
-        <div key={index}>
-          <p>{val["name"]}</p>
-          <p>{val["rate"]}</p>
-          <p>-{val["discount"]}</p>
-          <DiscountItemsDropdown itemList={val["items"]}/>
-        </div>
+        <CardBody key={index}>
+          <div>
+            <span>{val["name"]}</span>
+            <DiscountItemsDropdown discountName={val["name"]} itemList={val["items"]} rate={val["rate"]}/>
+          </div>
+          <span>{val["rate"]}</span>
+          <span>-{val["discount"]}</span>
+        </CardBody>
         )}
-      </div>
-      <div>
-        <span>
+      </BasketBox>
+      <FooterBox>
+        <Text>
           합계
-        </span>
-        <span>
+        </Text>
+        <Text style={{float:"right"}}>
           {total}
-        </span>
-      </div>
-      <button>다음</button>
+        </Text>
+      </FooterBox>
+      {/* <button>다음</button> */}
     </Body>
   )
 }
--- colavo/src/Pages/Basket/stlye.tsx
+++ colavo/src/Pages/Basket/stlye.tsx
@@ -1,28 +1,52 @@
 import sc from 'styled-components'
-import {Button} from 'antd'
 
 export const Body = sc.div`
-  margin-left: 30%;
-  margin-right: 30%;
+  margin-left: 34%;
+  margin-right: 34%;
   height: 800px;
   margin-top: 40px;
-  border: solid;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 5px 20px;
+  background: rgb(255, 255, 255);
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(239, 240, 244);
+  border-image: initial;
+  border-radius: 5px;
+  padding: 20px;
 `
 
 export const UserNameBox = sc.div`
   text-align: center;
-  border-bottom: solid;
+  border-bottom: 1.5px solid rgb(244, 247, 246);
 `
 
 export const ButtonBox = sc.div`
-  height: 40px;
-  padding: 10px;
+  height: 10%;
+  text-align: center;
+  padding: 10px
 `
-export const Antdbutton = sc(Button)`
+export const Antdbutton = sc.button`
   width: 40%;
   height: 50px;
-  border-radius: 10px;
+  border-radius: 20px;
+  border: 1px solid #eff0f4;
+  &: hover{
+    transform: scale(1.2);
+    transition: transform 0.2s linear;
+  }
 `
-// border: solid;
-// text-align: center;
-// padding: 20px;
+export const BasketBox = sc.div`
+  height: 70%;
+  border-bottom: 1.5px solid rgb(244,247,246);
+`
+
+export const FooterBox = sc.div`
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+`
+
+export const Text = sc.span`
+  font-weight: 700;
+  font-size: -webkit-xxx-large;
+`
